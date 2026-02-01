155cb96e94feb8003cd662c81b35d1d0e709f281
Alexander Akhmetov <me@alx.cx>
2026-02-01 18:31:14 +0100

    Fall back to runtime/debug VCS info for --version when ldflags are not set (#24)

--- cmd/programmator/main.go
+++ cmd/programmator/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"os"
+	"runtime/debug"
 
 	"github.com/worksonmyai/programmator/internal/tui"
 )
@@ -15,8 +16,49 @@ var (
 )
 
 func main() {
+	fillVersionFromBuildInfo()
 	tui.SetVersionInfo(version, commit, date)
 	if err := tui.Execute(); err != nil {
 		os.Exit(1)
 	}
+}
+
+func fillVersionFromBuildInfo() {
+	if version != "dev" {
+		return
+	}
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return
+	}
+	commit, date = versionFromSettings(info.Settings)
+}
+
+func versionFromSettings(settings []debug.BuildSetting) (string, string) {
+	var revision, date string
+	dirty := false
+	for _, s := range settings {
+		switch s.Key {
+		case "vcs.revision":
+			revision = s.Value
+		case "vcs.time":
+			date = s.Value
+		case "vcs.modified":
+			dirty = s.Value == "true"
+		}
+	}
+
+	c := "unknown"
+	if len(revision) >= 7 {
+		c = revision[:7]
+		if dirty {
+			c += "-dirty"
+		}
+	}
+
+	d := "unknown"
+	if date != "" {
+		d = date
+	}
+	return c, d
 }
